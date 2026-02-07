77f76d9269ac68bb8b02441f68815daecf0974dc
shahnazbegam854205-lab <shahnazbegam854205@gmail.com>
2026-02-07 23:37:25 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,40 +1,23 @@
-const CACHE_NAME = 'payment-announcer-v2';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-];
+// Service Worker
+const CACHE_NAME = 'payment-app-v1';
 
-// Install Service Worker
 self.addEventListener('install', event => {
+    console.log('Service Worker installing');
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => cache.addAll(['/']))
+            .then(() => self.skipWaiting())
     );
 });
 
-// Activate Service Worker
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+    console.log('Service Worker activating');
+    event.waitUntil(self.clients.claim());
 });
 
-// Fetch requests
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                // Return cached version or fetch from network
-                return response || fetch(event.request);
-            })
+            .then(response => response || fetch(event.request))
     );
 });
